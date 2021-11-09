858b76b02e4c178c50be98cfcffbe2024406f40d
Gabriel <gabriel.ashkenazi1@gmail.com>
2021-11-09 17:18:45 -0500

    Started TeleOp Mecanum Drive

---
+++ TeamCode/src/main/java/TeleOp/NovemberTele.java
@@ -0,0 +1,31 @@
+package TeleOp;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Mecanum Drive")
+public class NovemberTele extends LinearOpMode{
+    @Override public void runOpMode() {
+
+        MecanumDrive mecanumDrive = new MecanumDrive();
+
+        DcMotor fl = hardwareMap.dcMotor.get("fl");
+        DcMotor fr = hardwareMap.dcMotor.get("fr");
+        DcMotor bl = hardwareMap.dcMotor.get("bl");
+        DcMotor br = hardwareMap.dcMotor.get("br");
+
+        fl.setDirection(DcMotorSimple.Direction.FORWARD);
+        bl.setDirection(DcMotorSimple.Direction.FORWARD);
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        waitForStart();
+    }
+}
+
+class MecanumDrive {
+    DcMotor fl, fr, bl, br;
+}
