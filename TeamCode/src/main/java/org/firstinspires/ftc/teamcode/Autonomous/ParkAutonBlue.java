3a58ba56ba784466aa9ec6be1e7635c66b1abfb5
Gabriel <gabriel.ashkenazi1@gmail.com>
2021-11-20 01:16:51 -0500

    Autonomous Added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ParkAutonBlue.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.utils.MecanumDrive;
+
+@Autonomous(name = "ParkAutonRed")
+public class ParkAutonBlue extends LinearOpMode {
+    public void runOpMode() {
+        MecanumDrive mecDrive = new MecanumDrive(hardwareMap);
+
+        waitForStart();
+
+        mecDrive.move(1,0, Math.toRadians(-90));
+        sleep(250);
+        mecDrive.move(0,0, 0);
+    }
+}
