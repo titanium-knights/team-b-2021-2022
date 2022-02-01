643cf1767694cacc521c58b3b0acb00bfc70d706
Gabriel <gabriel.ashkenazi1@gmail.com>
2022-02-01 08:13:08 -0500

    FebruaryBranch Meet

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedDucksAuton.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "RedDucksAuton")
+public class RedDucksAuton extends LinearOpMode {
+    public void runOpMode() {
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FebruaryTele.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Feb Tele")
+public class FebruaryTele extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
