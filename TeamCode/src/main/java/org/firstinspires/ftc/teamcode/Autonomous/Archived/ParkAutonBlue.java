d75f5685cfec54c2d058e70e910206e3e23059b3
gerinome <danielgtravels@gmail.com>
2022-02-11 16:17:35 -0500

    Added blue auton for warehouse side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Archived/ParkAutonBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Archived/ParkAutonBlue.java
@@ -4,52 +4,54 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.utils.Arm;
+import org.firstinspires.ftc.teamcode.utils.Claw;
 import org.firstinspires.ftc.teamcode.utils.MecanumDrive;
 
+//blue block side
 @Autonomous(name = "ParkAutonBlue")
 public class ParkAutonBlue extends LinearOpMode {
     public void runOpMode() {
-        MecanumDrive mecDrive = new MecanumDrive(hardwareMap);
+        int armnum = 1;
+        //Grab freight
+        Claw claw = new Claw(hardwareMap);
+        claw.close();
 
-        // fly wheel
-        DcMotor carousel = hardwareMap.dcMotor.get("carousel");
+        //lift
+        //determine how high arm has to go to reach the top
+        Arm arm = new Arm(hardwareMap);
+        arm.up(armnum);
 
-        waitForStart();
+        //approach and drop into top level of the station
+        MecanumDrive robot = new MecanumDrive(hardwareMap);
 
-        // robot will start facing the warehouse
+        //start - turn to middle
+        robot.move(0, 0, 0.25);
+        //one tile, middle
+        robot.move(1, 0, 0);
+        //turn, face hub
+        robot.move(0, 0, -0.25);
+        //move to hub
+        robot.move(0, 1, 0);
+        //release, close, move away, lower
+        claw.open();
+        claw.close();
+        robot.move(0, -1, 0);
+        arm.down(armnum);
+        //turn towards ducks
+        robot.move(0, 0, 0.25);
+        //move one, turn 1/2 right, move to storage
+        robot.move(0, 1, 0);
+        robot.move(0, 0, -0.125);
+        robot.move(0, 1.44, 0);
+        robot.move(0, 0, 0.125);
 
-        mecDrive.move(0, -0.3, 0);
-        sleep(2000);
-        mecDrive.move(0,0,0);
-        carousel.setPower(.3);
 
-        /*
 
-            if the top moves too far:
-            mecDrive.move(0, -0.15, 0);
-            sleep(2000);
-            mecDrive.mpove(0,0,0);
-            carousel.setPower(.3);
 
-         */
 
-        sleep(2000);
-        carousel.setPower(0);
 
-        sleep(2000);
-        mecDrive.move(0,.6,0);
-        sleep(4000);
-        mecDrive.move(0,0, 0);
 
-        /*
-
-        if this moves too far forward:
-
-        sleep(2000);
-        mecDrive.move(0,.3,0);
-        sleep(4000);
-        mecDrive.move(0,0, 0);
-
-         */
     }
 }
