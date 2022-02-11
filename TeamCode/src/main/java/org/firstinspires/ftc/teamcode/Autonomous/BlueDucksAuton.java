8606dc96a3a2648fc829f6f87daccaff1abaeb4a
gerinome <danielgtravels@gmail.com>
2022-02-11 17:20:20 -0500

    Does carousel, moves to parking space

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueDucksAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueDucksAuton.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.utils.Arm;
 import org.firstinspires.ftc.teamcode.utils.Claw;
 import org.firstinspires.ftc.teamcode.utils.MecanumDrive;
@@ -11,35 +12,24 @@ import org.firstinspires.ftc.teamcode.utils.MecanumDrive;
 @Autonomous(name = "BlueDucksAuton")
 public class BlueDucksAuton extends LinearOpMode {
     public void runOpMode() {
-        int armnum = 1;
-        //Grab freight
-        Claw claw = new Claw(hardwareMap);
-        claw.close();
+        DcMotor carousel;
+        carousel = hardwareMap.dcMotor.get("carousel");
 
-        //lift
-        //determine how high arm has to go to reach the top
-        Arm arm = new Arm(hardwareMap);
-        arm.up(armnum);
 
         //approach and drop into top level of the station
         MecanumDrive robot = new MecanumDrive(hardwareMap);
 
-        //start - turn to middle
-        robot.move(0, 0, -0.25);
-        //one tile, middle
-        robot.move(.1, 0, 0);
-        //turn, face hub
-        robot.move(0, 0, 0.25);
-        //move to hub
-        robot.move(0, .1, 0);
-        //release, close, move away, lower
-        claw.open();
-        claw.close();
-        robot.move(0, -.15, 0);
-        arm.down(armnum);
+        //start - turn to carousel
+        robot.move(0, 0, -0.557);
+        //move to carousel
+        robot.move(0, -1.2, 0);
+        //spin carousel
 
-        // CAROUSEL PART STARTS NOW
-        //turn towards carousel
 
+        carousel.setPower(4);
+        sleep(2000);
+
+        //move diagonally into parking space
+        robot.move(0.1, 0.02, 0);
     }
 }
